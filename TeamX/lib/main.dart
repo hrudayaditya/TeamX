c3554fdc2f793ab598d97ad936f64f8af8ada322
swetha4444 <swethasaseendran4@gmail.com>
2025-05-12 15:48:17 -0400

    Add routes for LoginScreen and FantasyPointsSystem in MyApp

--- TeamX/lib/main.dart
+++ TeamX/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:teamx/view/fantasy/fantasy_points_system.dart';
 import 'package:teamx/view/splash/splash_screen.dart';
+import 'package:teamx/view/login/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,11 +9,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TeamX',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/fantasyPointsSystem': (context) => const FantasyPointsSystem(),
+      },
       home: const SplashScreen(),
     );
   }
