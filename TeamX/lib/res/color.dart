f6ebada524d43b514f8cad19e6595688e6e19123
hrudayaditya <hrudayaditya111@gmail.com>
2025-05-13 21:45:58 -0400

    Removed Unused constants

--- TeamX/lib/res/color.dart
+++ TeamX/lib/res/color.dart
@@ -1,21 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color homeBackground = Color(0xa6f4f5fb);
-  static const Color blackColor = Color(0x0ff00000);
   static const Color black = Colors.black;
-  static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color white = Colors.white;
-  static const Color buttonColor = Colors.green;
-  static const Color grey = Colors.grey;
-  static const Color red = Colors.red;
-  static const Color bloodRed = Color(0xffb71c1c);
-  static Color greyBackground = Colors.grey[300]!;
   static const Color green = Colors.green;
-  static Color lightRed = Colors.red[200]!;
-  static Color veryLightRed = Colors.red[200]!;
-  static const Color bottomSheetBuyColor = Colors.blue;
-  static const Color bottomSheetSellColor = Colors.red;
   static Color primaryColor = Colors.blue[900]!;
   static Color secondaryColor = Colors.grey[850]!;
   static Color lightGreen = Colors.green[200]!;
